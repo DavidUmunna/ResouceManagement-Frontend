5a9428a193281a4b5d5408199581d949a5f76999
David Umunna <chimarokeumunna98@gmail.com>
2025-10-18 16:06:42 +0100

    remove quantity field from createorder

--- src/pages/CreateOrder.jsx
+++ src/pages/CreateOrder.jsx
@@ -344,7 +344,7 @@ const CreateOrder = () => {
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
                   </div>
-                  <div>
+                 { /*<div>
                   <label>Quantity</label>
                   <input
                     type="number"
@@ -354,7 +354,7 @@ const CreateOrder = () => {
                     required
                     className="w-full px-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
                     />
-                  </div>
+                  </div>*/}
                   <div>
                   <label>Unit Price(Or Total)</label>
                   <input
